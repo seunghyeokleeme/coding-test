d1f2200f341c2d61010f8c29938d5138324655c4
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-06-21 20:28:59 +0900

    Update lv1 명예의 전당 시간복잡도 고려한 풀이

--- pg/lv1/명예의 전당/app.js
+++ pg/lv1/명예의 전당/app.js
@@ -1,4 +1,16 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/138477 
+// 시간복잡도 고려한 풀이
+function solution(k, score) {
+    let result = [], arr = [];
+    for(const today of score){
+        arr.push(today);
+        arr.sort((a, b) => a - b);
+        if(arr.length > k) arr.shift();
+        result.push(arr[0]);
+    }
+    return result;
+}
+/*
 function solution(k, score) {
     let result = [];
     for(let i=0; i < score.length; i++){
@@ -8,3 +20,4 @@ function solution(k, score) {
     }
     return result;
 }
+*/
